0b2b620c50656b9b7d2996767fabc3189826bcfe
Yog Sharma <yogthesharma@gmail.com>
2022-11-01 22:08:23 +0530

    reverse print table

---
+++ unusualCode/reverseprinttable.js
@@ -0,0 +1,17 @@
+function fn() {
+  var n = 10;
+  var a = n;
+  var b = 1;
+
+  while (a >= 1) {
+    while (b <= 10) {
+      console.log(`${b} x ${a} = ${b * a}`);
+      b++;
+    }
+    console.log(" ");
+    a--;
+    b = 1;
+  }
+}
+
+fn();
