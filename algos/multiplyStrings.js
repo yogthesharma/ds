4059eb8178bb411d84bef14a8a513d7574419146
Yog Sharma <yogthesharma@gmail.com>
2022-11-08 23:12:16 +0530

    started with multiply strings/not completed yet

---
+++ algos/multiplyStrings.js
@@ -0,0 +1,12 @@
+var multiply = function (num1, num2) {
+  num1 = num1 * 1;
+  num2 = num2 * 1;
+
+  const res = `${num1 * num2}`;
+  return res;
+};
+
+console.log(multiply("3", "4"));
+
+// this is not as simple as it looks
+// keep trying to understand this problem
