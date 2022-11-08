bdb043a5d0b326508d6c8aa1e1412f3a78a586a1
Yog Sharma <yogthesharma@gmail.com>
2022-11-08 11:26:18 +0530

    divide two integer self implementation

--- algos/divideTwoIntegers.js
+++ algos/divideTwoIntegers.js
@@ -1,7 +1,8 @@
-var divide = function (dividend, divisor) {
-  const subDivide = (a, b) => {
-    let q = 0;
+const divide = function (dividend, divisior) {
+  const subdivide = (a, b) => {
     let tmp = 0;
+    let q = 0;
+
     while (tmp <= a) {
       tmp += b;
       q++;
@@ -9,43 +10,44 @@ var divide = function (dividend, divisor) {
     q--;
     return [q, a - tmp + b];
   };
-
   let _dividend = Math.abs(dividend).toString().split("");
-  let di = Math.abs(divisor);
-  let _divisor = di.toString().split("");
+  let _divisior = Math.abs(divisior);
+
   let tmp = "";
   let result = "";
   let flag = false;
+
   while (true) {
     if (!_dividend.length) {
       break;
     }
     tmp = tmp + _dividend.shift();
     let _tmp = parseInt(tmp);
-    if (_tmp < di) {
+
+    if (_tmp < _divisior) {
       if (flag) {
         result = result + "0";
       }
     } else {
       flag = true;
-      let [q, left] = subDivide(_tmp, di);
+      let [q, left] = subdivide(_tmp, _divisior);
       result = result + q;
       if (left > 0) {
         tmp = left + "";
       } else {
-        tmp = "";
+        tmp = 0;
       }
     }
   }
 
-  let quotient = parseInt(result) || 0;
-  if (quotient > 0) {
-    return (dividend > 0) ^ (divisor > 0)
-      ? -quotient
-      : Math.min(Math.pow(2, 31) - 1, quotient);
+  const answer = parseInt(result) || 0;
+  if (answer) {
+    return (dividend > 0) ^ (divisior > 0)
+      ? -answer
+      : Math.min(Math.pow(2, 31) - 1, answer);
   } else {
     return 0;
   }
 };
 
-console.log(divide(-2147483648, -1));
+console.log(divide(50, 5));
